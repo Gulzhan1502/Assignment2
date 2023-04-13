a31b54ab94b2d9fae9cc3ba3fcd94eac995bef13
gulzanutekina <gulzhan.utekina@gmail.com>
2023-04-14 02:24:57 +0600

    boolean add(E element)

---
+++ src/List.java
@@ -0,0 +1,4 @@
+import java.util.*;
+public class List<E> extends MyArrayList<E>{
+    boolean add(E element);
+}
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,20 @@
-public class MyArrayList {
-    
+import java.util.*;
+public class MyArrayList<E> implements List<E> {
+    private Object [] elements;
+    private int size;
+    public MyArrayList(){
+        elements=new Object[10];
+        size=0;
+    }
+    public boolean add(E element){
+        if (size==elements.length){
+            int newSize=elements.length *2;
+            elements=Arrays.copyOf(elements,newSize);
+        }
+
+        elements[size]=element;
+        size++;
+        return true;
+    }
+
 }
